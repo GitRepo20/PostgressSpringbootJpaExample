16ae2f73eebaced8e3a4efc2bfb40dde2b2f135c
Santosh <Santosh@LAPTOP-T07B1BV6>
2020-04-23 15:20:45 +0530

    detete

--- SpringbootJPAPostgresPOC/src/main/java/com/example/demo/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.dao;
-
-public interface UserDao {
-	public void createUser();
-	public void createProfile();
-	public void createProfileMovies();
-}
--- SpringbootJPAPostgresPOC/src/main/java/com/example/demo/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-public interface UserService {
-
-	public void createUser();
-	public void createProfile();
-	public void createProfileMovies();
-}
